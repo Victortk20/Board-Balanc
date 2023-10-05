453bb1436a750b16132a84d0c22eb427fa986a2f
CarlosWGama <carloswgama@gmail.com>
2023-10-05 18:07:39 -0300

    Ajustado bluetooth

--- Posição_1/index.tsx
+++ Posição_1/index.tsx
@@ -12,6 +12,7 @@ import RNBluetoothClassic, {
 } from 'react-native-bluetooth-classic';
 
 import React, { useState, useEffect } from 'react';
+import Grafico from '../components/Grafico';
 
 
 export interface BemvindoScreenProps {
@@ -22,8 +23,14 @@ export interface BemvindoScreenProps {
 export  function Pagina1 (props: any) {
 
   const navigation = useNavigation<any>();
-  const [deviceAddress, setDeviceAddress] = useState<string>('60:A5:E2:EB:C2:0F'); // Substitua pelo endereço do dispositivo que você deseja verificar
+  const [ points, setPoints ] = useState<{x:number, y:number}[]>([]);
+  const [ x, setX ] = useState<number|null>(null);
+  const [ y, setY ] = useState<number|null>(null);
+  const [ z, setZ ] = useState<number|null>(null);
+  
+  const deviceAddress = '00:21:13:03:1B:4A'; // Substitua pelo endereço do dispositivo que você deseja verificar
   const [isConnected, setIsConnected] = useState<boolean>(false);
+  const delaySegundos = 5;
   
   const checkDeviceConnection = async () => {
     try {
@@ -43,28 +50,49 @@ export  function Pagina1 (props: any) {
       //Permissão liberada
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
 
-        //Busca os dispositivos conectados
-        const connected = await RNBluetoothClassic.getBondedDevices();
-        connected.forEach(async device => {
-          //conecta com o aplicativo
-          if (device.address == deviceAddress)
-            await device.connect();
-          
-          console.log(device.address);
-          console.log(device.name)
-          const conectado = await device.isConnected();
-          if (conectado) {
-            //REALIZAR AQUI A RECUPERAÇÃO DOS DADOS ENVIADO PELO DISPOTIVO!!!
-            device.onDataReceived(data => {
-              console.log(data)
-            })
-          }
-        })
+
+        let device = await RNBluetoothClassic.connectToDevice(deviceAddress);  // Returns BlutoothDevice
+        if (await device.isConnected()) {
+          setInterval(async () => {
+            let dados:any = await device.read();
+            //recuperando X
+            dados = dados.substring(1);
+            dados = dados.split('Y');
+            const x = dados[0];
+            
+            //recuperando Y
+            dados = dados[1].split('Z');
+            const y = dados[0];
+
+            //Recuperando Y
+            const z = dados[1];
+
+            console.log('Dados');
+            console.log('- X', x/100);
+            console.log('- Y', y/100);
+            console.log('- Z', z/100);
+            setX(x/100);
+            setY(y/100);
+            setZ(z/100);
+          }, delaySegundos * 1000);
+
+        }
+        
+        
       }
     } catch (error) {
       console.error('Erro ao verificar a conexão Bluetooth:', error);
     }
   };
+
+  useEffect(() => {
+    if (y != null) {
+      const newPoints = [...points];
+      newPoints.push({x, y});
+      console.log(newPoints);
+      setPoints(newPoints);
+    }
+  }, [y])
 
   useEffect(() => {
     // Chama a função de verificação ao montar o componente
@@ -74,19 +102,20 @@ export  function Pagina1 (props: any) {
   
   return (
     <>
-    <View style={styles.container}>
-    <Text style={styles.titulo}>Posição 1</Text>
-    <View style={styles.container2}>
-    <Text>Grafico 1</Text>
-    </View >
-    
-    <Text style={styles.subtitulo}>Resultado:</Text>
-    <Text style={styles.subtitulo}>Média:</Text>
-    <Text style={styles.subtitulo}>Tempo do Teste:</Text>
-    <TouchableOpacity style={styles.botao} onPress={() => navigation.goBack()}>
-        <Text style={styles.textobotao}>Voltar</Text>
-    </TouchableOpacity>
-    </View>
+      <View style={styles.container}>
+          <Text style={styles.titulo}>Posição 1</Text>
+          <View style={styles.container2}>
+          <Text>Grafico 1</Text>
+          <Grafico points={points}/>
+      </View >
+      
+      <Text style={styles.subtitulo}>Resultado:</Text>
+      <Text style={styles.subtitulo}>Média:</Text>
+      <Text style={styles.subtitulo}>Tempo do Teste:</Text>
+      <TouchableOpacity style={styles.botao} onPress={() => navigation.goBack()}>
+          <Text style={styles.textobotao}>Voltar</Text>
+      </TouchableOpacity>
+      </View>
 
     {/*
     <View>
@@ -117,11 +146,10 @@ const styles = StyleSheet.create({
   container2: {
 
     justifyContent: 'center',
-    padding:40,
     backgroundColor:'#b1e3a3',
-    borderRadius: 5,
-    width: 150, 
-    height: 200,
+    borderRadius: 15,
+    width: 300, 
+    height: 450,
     marginLeft: 10,
     alignItems: 'center',
     
